33b87bff5379c347ab5549af622c48605b4da451
Chriss Mejía <me@chrissmejia.com>
2015-08-16 04:59:09 -0600

    Adding tabs v0.2

--- js/opensloth.js
+++ js/opensloth.js
@@ -22,5 +22,54 @@
 // SOFTWARE.                                                                              //
 ////////////////////////////////////////////////////////////////////////////////////////////
 
-$menu-width: 14rem;
-$side_content-width: 18rem;
+(function(){
+    "use strict";
+    // Foundation JavaScript
+    // Documentation can be found at: http://foundation.zurb.com/docs
+    $(document).foundation();
+
+    var side_content_tag = $('#side_content');
+    var side_content_close_tag = $('#side_content .close');
+    var inner_content_tag = $('#inner_content');
+    var top_side_content_object_tag = $('.side-content-buttons');
+    var top_side_content_button_tag = $('.side-content-buttons a');
+
+    window.OpenSloth = {
+        name : 'OpenSloth',
+        version : '0.1',
+        
+        header : {
+            side_content_buttons : {
+                show : function() {
+                    top_side_content_object_tag.removeClass("hidden");
+                },
+                hide : function() {
+                    top_side_content_object_tag.addClass("hidden");
+                },
+            }
+        },
+        side_content : {
+        	width : side_content_tag.width(),
+
+            show : function() {
+                inner_content_tag.removeClass("full");
+                OpenSloth.header.side_content_buttons.hide();
+            },
+        	hide : function() {
+                inner_content_tag.addClass("full");
+                OpenSloth.header.side_content_buttons.show();
+        	},
+        }
+
+    }
+    OpenSloth.side_content.hide();
+
+    top_side_content_button_tag.click(function(){
+        var target_tab = $(this).data('tab');
+        $('.tab-title a[aria-controls="' + target_tab + '"]').trigger( "click" );
+        OpenSloth.side_content.show();
+    });
+    side_content_close_tag.click(function(){
+        OpenSloth.side_content.hide();
+    });
+})();
